0c0b568e5588619ffaffce2f72fc07526ce6fd58
Santiago Narvaez <santiagonar1@gmail.com>
2024-09-30 15:53:22 +0200

    refactor(tests): import namespace playground::parser

--- tests/parser_test.cpp
+++ tests/parser_test.cpp
@@ -3,6 +3,8 @@
 
 #include <parser.hpp>
 
+using namespace playground::parser;
+
 TEST(IsHeader, ReturnsTrueIfLineIsHeader) {
     const auto header_line = std::string{"****** reading METIS data form time= 30 mins *********"};
     EXPECT_TRUE(playground::parser::is_header(header_line));
@@ -10,5 +12,5 @@ TEST(IsHeader, ReturnsTrueIfLineIsHeader) {
 
 TEST(IsHeader, ReturnsFalseIfLineIsNotHeader) {
     const auto data_line = std::string{"timeMpiSend=[0,]"};
-    EXPECT_FALSE(playground::parser::is_header(data_line));
+    EXPECT_FALSE(is_header(data_line));
 }
